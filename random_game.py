dbe4e2ba54e5ec9cd7ce96e38533e379ed3f07d6
Bharath24-10 <bharathbalachandran24@gmail.com>
2025-02-02 19:26:37 +0530

    game

---
+++ random_game.py
@@ -0,0 +1,21 @@
+import random
+
+number_guessed=random.randrange(100)
+
+number_of_chance=3
+
+guessed=0
+
+while guessed > number_of_chance:
+    guessed+=1
+    n=int(input("enter the number:"))
+    
+    if n==number_guessed:
+        print("u guessed right")
+        break
+    elif(guessed>=number_guessed and n!=number_guessed):
+        print(f"chance is over and the number is {number_guessed}")
+    elif n>number_guessed:
+        print("the number is high")
+    elif n<number_guessed:
+        print("the number is lower")
